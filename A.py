54ff541bffc83060c50d6fe9feb1c19ef4f3e634
DN-Blue <53571576+DN-Blue@users.noreply.github.com>
2020-01-11 14:36:07 +0700

    Delete
    Delete!

--- A.py
+++
@@ -1,9 +0,0 @@
-NamSinh=input('nhap nam sinh : ')
-    
-    
-Thoigian=input('bay gio la luc nao ? ')
-    
-
-
-
-#('so tuoi:',int(Thoigian)-int(NamSinh))
--- CtoF.py
+++
@@ -1,2 +0,0 @@
-c=input('nhap do C:')
-print('do F:',int(c)*1.8+32)
